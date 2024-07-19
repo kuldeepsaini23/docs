3d0728bbac04784db6de1dbadafa76737586a2bf
kuldeepsaini23 <kuldeepsaini02311@gmail.com>
2024-07-20 00:58:41 +0530

    setup:clerk

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,10 +1,26 @@
+import { Editor } from '@/components/editor/Editor'
+import Header from '@/components/Header'
+import { SignedOut, SignedIn, SignInButton, UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 type Props = {}
 
 const Document = (props: Props) => {
   return (
-    <div>Document</div>
+    <div>
+      <Header>
+        <div className='flex w-fit items-center justify-center gap-2'>
+          <p className='document-title'>This is a Fake document title</p>
+        </div>
+        <SignedOut>
+          <SignInButton/>
+        </SignedOut>
+        <SignedIn>
+          <UserButton/>
+        </SignedIn>
+      </Header>
+      <Editor/>
+    </div>
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,44 @@
-import { Inter as FontSans } from "next/font/google"
+import { Inter as FontSans } from "next/font/google";
 
-import { cn } from "@/lib/utils"
-import React from "react"
-import { Metadata } from "next"
-import './globals.css'
+import { cn } from "@/lib/utils";
+import React from "react";
+import { Metadata } from "next";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
-export const metadata:Metadata  = {
+export const metadata: Metadata = {
   title: "LiveDocs",
   description: "Your go-to collaborative editor",
-}
+};
 
-export default function RootLayout({ children }: {children:React.ReactNode}) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        {children}
-      </body>
-    </html>
-  )
+    <ClerkProvider appearance={{
+      baseTheme:dark,
+      variables:{colorPrimary:"#3371FF", fontSize:"16px"},
+      
+    }}>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
 }
